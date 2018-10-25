2c0871afeef1665e5a1f7d63245b509ca54930fa
TREXplode <doug.huni@gmail.com>
2018-10-25 09:52:03 -0700

    Lab4

---
+++ Lab4CST465/Lab4CST465/Models/ComputerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4CST465.Models
+{
+    public class ComputerModel
+    {
+        public string Name { get; set; }
+        public string AdministratorPassword { get; set; }
+        public string IPAddress { get; set; }
+        public string Location { get; set; }
+        public string OS { get; set; }
+        public string Description { get; set; }
+        public int SupportedMonitors { get; set; }
+
+        public static readonly List<string> OSTypes = new List<string> { "IOS", "Windows", "Linux" };
+    }
+}
